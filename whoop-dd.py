9c320c0f2396554b5e5b0cafe1df9fdd5b36cc0d
avadnais <andy.vadnais@datadoghq.com>
2023-01-23 11:05:07 -0700

    removed api logging

--- whoop-dd.py
+++ whoop-dd.py
@@ -12,7 +12,6 @@ logHandler.setFormatter(formatter)
 logger.addHandler(logHandler)
 logger.setLevel(logging.INFO)
 
-
 load_dotenv()
 
 username = os.getenv("USERNAME") or ""
@@ -63,34 +62,3 @@ while True:
     logger.info(cycles[0])
     
     time.sleep(30)
-    
-""" 
-API LOGGING 
-"""
-"""
-Send deflate logs returns "Request accepted for processing (always 202 empty JSON)." response
-"""
-
-# from datadog_api_client import ApiClient, Configuration
-# from datadog_api_client.v2.api.logs_api import LogsApi
-# from datadog_api_client.v2.model.content_encoding import ContentEncoding
-# from datadog_api_client.v2.model.http_log import HTTPLog
-# from datadog_api_client.v2.model.http_log_item import HTTPLogItem
-
-# body = HTTPLog(
-#     [
-#         HTTPLogItem(
-#             ddsource="python",
-#             ddtags="env:dev",
-#             message=str(cycles[0]),
-#             service="whoop",
-#         ),
-#     ]
-# )
-
-# configuration = Configuration()
-# with ApiClient(configuration) as api_client:
-#     api_instance = LogsApi(api_client)
-#     response = api_instance.submit_log(content_encoding=ContentEncoding.DEFLATE, body=body)
-
-#     print(response)
