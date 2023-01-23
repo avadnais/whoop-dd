2899f029de488074b99573d24284e8b9f6c5959a
avadnais <andy.vadnais@datadoghq.com>
2023-01-23 11:04:21 -0700

    refactor

--- whoop-dd.py
+++ whoop-dd.py
@@ -40,29 +40,30 @@ while True:
     print("--- PROFILE ---")
     print(profile)
     print()
+    logger.info(profile)
+
     print("--- BODY MEASUREMENTS ---")
     print(body)
     print()
+    logger.info(body)
+
     print("--- SLEEP ----")
     print(sleeps[0])
     print()
+    logger.info(sleeps[0])
+
     print("--- RECOVERY ---")
     print(recoveries[0])
     print()
+    logger.info(recoveries[0])
+
     print("--- CYCLE ---")
     print(cycles[0])
     print()
-    logger.info(profile)
     logger.info(cycles[0])
-    logger.info(sleeps[0])
-    logger.info(recoveries[0])
+    
     time.sleep(30)
     
-
-
-
-
-
 """ 
 API LOGGING 
 """
